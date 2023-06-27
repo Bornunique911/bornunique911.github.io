3abfed706265c7392a2e133bde090fe6101edcce
Bornunique911 <69379200+Bornunique911@users.noreply.github.com>
2023-06-28 02:56:12 +0530

    Add files via upload

---
+++ _site/assets/js/data/swcache.js
@@ -0,0 +1,46 @@
+const resource = [
+    /* --- CSS --- */
+    '/assets/css/style.css',
+
+    /* --- PWA --- */
+    '/app.js',
+    '/sw.js',
+
+    /* --- HTML --- */
+    '/index.html',
+    '/404.html',
+
+    
+        '/categories/',
+    
+        '/tags/',
+    
+        '/archives/',
+    
+        '/about/',
+    
+
+    /* --- Favicons & compressed JS --- */
+    
+    
+];
+
+/* The request url with below domain will be cached */
+const allowedDomains = [
+    
+
+    'localhost:4000',
+
+    
+
+    'fonts.gstatic.com',
+    'fonts.googleapis.com',
+    'cdn.jsdelivr.net',
+    'polyfill.io'
+];
+
+/* Requests that include the following path will be banned */
+const denyUrls = [
+    
+];
+

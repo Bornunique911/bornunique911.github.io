44e816414a35ec4abb160cfa624b52ca3d1bba9e
bornunique911 <raviraj.mehta911@gmail.com>
2023-07-12 03:17:45 +0530

    modified:   2022-03-07-gallery-tryhackme-write-up.html 	modified:   2022-10-16-payatu-hiring-ctf-2022-challenges-writeup.html 	modified:   2023-06-27-intigriti-s-june-xss-challenge-writeup.html 	modified:   ../_site/404.html 	modified:   ../_site/about/index.html 	modified:   ../_site/archives/index.html 	modified:   ../_site/assets/js/data/search.json 	modified:   ../_site/categories/index.html 	modified:   ../_site/feed.xml 	modified:   ../_site/index.html 	modified:   ../_site/posts/gallery-tryhackme-write-up/index.html 	modified:   ../_site/posts/intigriti-s-june-xss-challenge-writeup/index.html 	modified:   ../_site/posts/payatu-hiring-ctf-2022-challenges-writeup/index.html 	modified:   ../_site/sitemap.xml 	modified:   ../_site/sw.js 	modified:   ../_site/tags/index.html

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230712.030005';
+const cacheName = 'chirpy-20230712.031702';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {

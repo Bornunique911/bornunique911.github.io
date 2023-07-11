e4339ee1ae3ad0193efd3a5b034671b8d1f5eef1
bornunique911 <raviraj.mehta911@gmail.com>
2023-07-12 02:21:53 +0530

    modified:   _posts/2023-06-27-intigriti-s-june-xss-challenge-writeup.html 	modified:   _site/404.html 	modified:   _site/about/index.html 	modified:   _site/app.js 	modified:   _site/archives/index.html 	modified:   _site/assets/css/style.css 	modified:   _site/assets/css/style.css.map 	modified:   _site/assets/img/favicons/favicon-16x16.png 	modified:   _site/assets/img/favicons/favicon-32x32.png 	modified:   _site/assets/img/favicons/mstile-150x150.png 	modified:   _site/assets/js/data/search.json 	modified:   _site/categories/index.html 	modified:   _site/feed.xml 	modified:   _site/index.html 	modified:   _site/posts/gallery-tryhackme-write-up/index.html 	modified:   _site/posts/intigriti-s-june-xss-challenge-writeup/index.html 	modified:   _site/posts/payatu-hiring-ctf-2022-challenges-writeup/index.html 	modified:   _site/sitemap.xml 	modified:   _site/sw.js 	modified:   _site/tags/index.html

--- _site/app.js
+++ _site/app.js
@@ -40,4 +40,3 @@ if ('serviceWorker' in navigator) {
     });
 }
 
-
--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230627.225048';
+const cacheName = 'chirpy-20230712.022007';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {

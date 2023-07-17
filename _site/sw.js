29943889489f387a1767a4a94e4b44904cb1738a
bornunique911 <raviraj.mehta911@gmail.com>
2023-07-17 23:44:09 +0530

    Fix typo

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230717.233322';
+const cacheName = 'chirpy-20230717.234043';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {

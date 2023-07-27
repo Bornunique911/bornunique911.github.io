8b12b9a8adb8e9ca2e9b489e6361212490c2eb01
bornunique911 <raviraj.mehta911@gmail.com>
2023-07-27 16:02:54 +0530

    modified:   _posts/2023-06-27-intigriti-s-june-xss-challenge-writeup.html 	modified:   _site/404.html 	modified:   _site/about/index.html 	modified:   _site/archives/index.html 	modified:   _site/categories/index.html 	modified:   _site/feed.xml 	modified:   _site/index.html 	modified:   _site/posts/gallery-tryhackme-write-up/index.html 	modified:   _site/posts/intigriti-s-june-xss-challenge-writeup/index.html 	modified:   _site/posts/payatu-hiring-ctf-2022-challenges-writeup/index.html 	modified:   _site/sitemap.xml 	modified:   _site/sw.js 	modified:   _site/tags/index.html

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230727.153535';
+const cacheName = 'chirpy-20230727.160113';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
